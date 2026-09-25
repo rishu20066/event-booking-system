a902ee2d1079dab92e79c70c9dc2ec8667876622
rishu20066 <riteshsahani544@gmail.com>
2026-09-25 19:59:43 +0530

    Refactor database connection configuration

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306,        // <-- YEH ADD KARO
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'event_booking',
+  host: process.env.DB_HOST ,
+  port: process.env.DB_PORT ,       
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME ,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
